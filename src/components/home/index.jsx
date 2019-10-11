8799c328f86267d2e64799a62fa7e7736f7791b3
bigbigDreamer <1826001146@qq.com>
2019-10-11 19:17:19 +0800

    初始化项目，分治划分，组件拆分，路由拆分，redux拆分，mock数据模拟

---
+++ src/components/home/index.jsx
@@ -0,0 +1,16 @@
+import React,{Component}  from 'react'
+import { Row, Col } from 'antd';
+
+export default class Home extends Component {
+    state = {
+
+    };
+    render() {
+        return (
+          <div className="home">
+
+          </div>
+        )
+    }
+
+}
